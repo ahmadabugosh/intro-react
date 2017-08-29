4bb72e05b36a851a7d81ff49ff22a22b35e3e8f6
Ahmad Abugosh <ahmadabugosh@gmail.com>
2017-08-30 01:24:09 +0400

    Finished starting up web app

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import {render} from 'react-dom';
-const ce = React.createElement;
+import { render } from 'react-dom';
 
-const myTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="Search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const myFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(myTitle, { title: 'Rick and Morty', color: 'YellowGreen' }),
-    ce(myTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(myTitle, { title: 'Better Call Saul', color: 'LimeGreen' })
-  );
-};
-
-render(ce(myFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
